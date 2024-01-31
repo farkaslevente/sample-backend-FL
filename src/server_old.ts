a405f91e73d35328565b5a35e16706269d4f5a0f
Farkas Levente <farkas.levente2004@gmail.com>
2024-01-31 14:28:06 +0100

    V1: 2024.01.31

---
+++ src/server_old.ts
@@ -0,0 +1,15 @@
+// import express, {Express, Request, Response} from "express";
+// import * as bodyParser from 'body-parser';
+
+// function loggerMiddleware(request: express.Request, response: express.Response, next) {
+//   console.log(`${request.method} ${request.path}`);
+//   next();
+// }
+ 
+// const app: Express = express();
+// app.use(bodyParser.json());
+// app.get('/', (request: Request, response: Response) => {
+//   response.send('Hello world!');
+// });
+ 
+// app.listen(5000);
